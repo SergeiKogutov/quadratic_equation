acdb709a6f686d16ae256db9730589d666512ba5
Sergei Kogutov <110593957+SergeiKogutov@users.noreply.github.com>
2024-03-09 16:31:20 +0700

    Update quadratic_equation.c

--- quadratic_equation.c
+++ quadratic_equation.c
@@ -5,7 +5,7 @@ double *solve_equation(double a, double b, double c) {
   double *res = (double *)malloc(2 * sizeof(double *));
   res[0] = 0.0;
   res[1] = 0.0;
-  D = pow(b, 2) - 4 * a * (-c);
+  D = pow(b, 2) - 4 * a * c;
   x = (-b - sqrt(D)) / (2 * a);
   res[0] = x;
   x = (-b + sqrt(D)) / (2 * a);
